1e220b3497fcf5ceeb73e67bd76f55408f60999a
Dan Rees <danrees8743@gmail.com>
2022-08-20 17:42:36 +0100

    added the of the functions

--- js/ratings.js
+++ js/ratings.js
@@ -3,8 +3,23 @@ function collect_ratings() {
     count: 0,
     sum: 0,
     average: 0,
-    collect_ratings: function () {
-      let rating = 0;
-    },
   };
+  let rating = 0;
+  const elements = document.querySelectorAll('.rating');
+  elements.forEach((element) => {
+    rating = parseTint(element.id.replace('star', ''));
+    ratings.count += parseTint(element.value);
+    ratings.sum += rating * parseTint(element.value);
+  });
+  if (ratings.count !== 0) {
+    ratings.average = ratings.sum / ratings.count;
+  }
+  return ratings;
 }
+collect_ratings();
+
+document.addEventListener('change', (event) => {
+  event.preventDefault();
+  const ratings = collect_ratings();
+  document.querySelector('#average').value = ratings.average.toFixed(2);
+});
